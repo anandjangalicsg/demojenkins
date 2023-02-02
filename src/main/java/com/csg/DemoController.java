05b2e8d9f95b3c9462bf7d4c31d61f7c86895190
Jangali Anand <anandjan@creative.local>
2023-02-02 22:05:24 +0530

    Adding new proj

---
+++ src/main/java/com/csg/DemoController.java
@@ -0,0 +1,12 @@
+package com.csg;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class DemoController {
+
+	@GetMapping("/")
+	public String getDemo() {
+		return "Spring Demo";
+	}
+}
